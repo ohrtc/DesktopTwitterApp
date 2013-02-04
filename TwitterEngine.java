c866364a47f499adde67d05ab5f76941b4af0a92
Cameron Ohrt <ohrtc@mail.gvsu.edu>
2013-02-04 11:28:26 -0500

    Tweet capability

--- TwitterEngine.java
+++ TwitterEngine.java
@@ -4,10 +4,25 @@
  * 
  */
 import twitter4j.*;
-public class TwitterEngine{
-	private Twitter engine;
-	public static void main(String[] args) {
 
+public class TwitterEngine {
+	private Twitter engine;
+
+	public TwitterEngine() {
+		engine = TwitterFactory.getSingleton();
+	}
+
+	public void updateStatus(String status) {
+		try {
+			engine.updateStatus(status);
+		} catch (TwitterException ex) {
+			System.out.println("Tweet failed.");
+		}
+	}
+
+	public static void main(String[] args) {
+		TwitterEngine test = new TwitterEngine();
+		test.updateStatus("Hello, test.");
 	}
 
 }
