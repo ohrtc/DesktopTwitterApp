9c3132348a71e9d90b1495e1fc0231a865f99d8d
Cameron Ohrt <ohrtc@mail.gvsu.edu>
2013-02-06 08:15:30 -0500

    Additional Features
    Tweeting, retweeting, search people, search tweets. No following yet.

--- TwitterEngine.java
+++ TwitterEngine.java
@@ -12,17 +12,54 @@ public class TwitterEngine {
 		engine = TwitterFactory.getSingleton();
 	}
 
-	public void updateStatus(String status) {
+	public Status updateStatus(String status) {
 		try {
-			engine.updateStatus(status);
+			Status result = engine.updateStatus(status);
+			return result;
 		} catch (TwitterException ex) {
 			System.out.println("Tweet failed.");
 		}
+		return null;
 	}
+
+	public Status retweet(long SID) {
+		try {
+			Status result = engine.retweetStatus(SID);
+			return result;
+		} catch (TwitterException ex) {
+			System.out.println("Retweet failed.");
+		}
+		return null;
+	}
+	
+	public ResponseList<User> searchPeople(String query, int numPages){
+		try {
+			ResponseList<User> results = engine.searchUsers(query, numPages);
+			return results;
+		} catch (TwitterException ex) {
+			System.out.println("Search failed.");
+		}
+		return null;
+	}
+	
+	public QueryResult searchTweets(String query){
+		try {
+			Query toSearch = new Query(query);
+			QueryResult results = engine.search(toSearch);
+			return results;
+		} catch (TwitterException ex) {
+			System.out.println("Search failed.");
+		}
+		return null;
+	}
+	
+	public void newmethod(){
+	}
+	
+	
 
 	public static void main(String[] args) {
 		TwitterEngine test = new TwitterEngine();
 		test.updateStatus("Hello, test.");
 	}
-
 }
